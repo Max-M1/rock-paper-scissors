05056b2bb0d8a6ec856b99c8e855e21c5fbf9d36
Max <mmaslov848@gmail.com>
2025-02-11 22:10:22 +0200

    Оновлено 12 лабораторну: використано функцію sum(), цикл for та блок try...except

--- PR12.py
+++ PR12.py
@@ -1,12 +1,16 @@
 def sum_numbers():
-    n = int(input("Введіть число: "))
+    try:
+        n = int(input("Введіть число: "))
 
-    if n < 1:
-        print("Будь ласка, введіть число більше або рівне 1.")
-        return
+        if n < 1:
+            print("Будь ласка, введіть число більше або рівне 1.")
+            return
 
-    total = sum(i for i in range(1, n + 1))
-    print(f"Сума чисел від 1 до {n} дорівнює {total}")
+        total = sum(i for i in range(1, n + 1))
+        print(f"Сума чисел від 1 до {n} дорівнює {total}")
+
+    except ValueError:
+        print("Помилка: введіть ціле число.")
 
 
 if __name__ == "__main__":
